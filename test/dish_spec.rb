575f882e9a3c7113afe266dda5b34e50aecf0720
philmarcu <philmark18@gmail.com>
2022-05-17 13:21:33 -0600

    Feat: complete Iteration 1

---
+++ test/dish_spec.rb
@@ -0,0 +1,12 @@
+require './lib/dish'
+
+RSpec.describe Dish do
+  dish = Dish.new("Couscous Salad", :appetizer)
+
+  it 'exists' do
+    expect(dish). to be_a(Dish)
+    expect(dish.name).to eq("Couscous Salad")
+    expect(dish.category).to eq(:appetizer)
+  end
+
+end
